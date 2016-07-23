06f12c9c2d7b904090d55f6b505b556b9b855ff5
Lajule <julien.rouzieres@mac.com>
2016-07-23 16:41:00 +0200

    initialize repository

---
+++ platforms/ios/Starter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Starter
+//
+//  Created by Julien Rouzieres on 23/07/2016.
+//  Copyright © 2016 Julien Rouzieres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
