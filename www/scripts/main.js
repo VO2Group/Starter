6f3aff3da2c6bc9d0aa8bc4ba3a0f4b4e8e50984
Lajule <julien.rouzieres@mac.com>
2016-07-28 00:08:43 +0200

    localStorage

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -1,7 +1,15 @@
 document.onreadystatechange = function () {
-  var hello = document.getElementById('hello');
-  hello.addEventListener('click', function () {
-    var placeholder = document.getElementById('placeholder');
-    placeholder.innerHTML = 'hello';
-  });
+  if (document.readyState == 'interactive') {
+    var n = Number(window.localStorage.getItem('counter') || 0) + 1;
+    window.localStorage.setItem('counter', n);
+
+    var counter = document.getElementById('counter');
+    counter.innerHTML = n;
+
+    var hello = document.getElementById('hello');
+    hello.addEventListener('click', function () {
+      var placeholder = document.getElementById('placeholder');
+      placeholder.innerHTML = 'world!';
+    });
+  }
 };
