69a9d04764156ec142f6aa872fc63f037c7d1263
Lajule <julien.rouzieres@mac.com>
2016-08-19 18:35:26 +0200

    bad indent and ios title bar

--- platforms/ios/AppShell/ViewController.swift
+++ platforms/ios/AppShell/ViewController.swift
@@ -10,41 +10,45 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
-    
+
     var webView: WKWebView?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let contentController = WKUserContentController()
         contentController.addScriptMessageHandler(self, name: "handler")
         let config = WKWebViewConfiguration()
         config.userContentController = contentController
-        
+
         self.webView = WKWebView (frame: self.view.frame, configuration: config)
         self.webView!.navigationDelegate = self
         self.webView!.scrollView.bounces = false
         self.webView!.allowsBackForwardNavigationGestures = true
         self.view.addSubview(self.webView!)
-        
-        let starter = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("platform", ofType: "js")!)
-        self.webView!.evaluateJavaScript(try! String(contentsOfURL: starter), completionHandler: nil)
-        
-        let www = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("index", ofType: "html", inDirectory: "www")!)
-        self.webView!.loadFileURL(www, allowingReadAccessToURL: www.URLByDeletingLastPathComponent!)
-        
+
+        let platform = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("platform", ofType: "js")!)
+        self.webView!.evaluateJavaScript(try! String(contentsOfURL: platform), completionHandler: nil)
+
+        let index = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("index", ofType: "html", inDirectory: "www")!)
+        self.webView!.loadFileURL(index, allowingReadAccessToURL: index.URLByDeletingLastPathComponent!)
+
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.onPause), name: UIApplicationDidEnterBackgroundNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.onResume), name: UIApplicationWillEnterForegroundNotification, object: nil)
     }
-    
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+
     func onPause(notification: NSNotification) {
         self.webView!.evaluateJavaScript("document.dispatchEvent(new Event('pause'));", completionHandler: nil)
     }
-    
+
     func onResume(notification: NSNotification) {
         self.webView!.evaluateJavaScript("document.dispatchEvent(new Event('resume'));", completionHandler: nil)
     }
-    
+
     func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
         if message.name == "handler" {
             switch message.body["method"] as! String {
@@ -59,13 +63,13 @@ class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHan
             }
         }
     }
-    
+
     func foo(message: String) {
         let alert = UIAlertController(title: "Foo", message: message, preferredStyle: UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
         self.presentViewController(alert, animated: true, completion: nil)
     }
-    
+
     func bar(message: String, callback: String) {
         let alert = UIAlertController(title: "Bar", message: message, preferredStyle: UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) in
